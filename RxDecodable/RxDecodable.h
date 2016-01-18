8da80ad3295aaa5b900f8f10a1ac811180e121c7
alexperezpaya <alex@ermesup.com>
2016-01-18 18:05:23 +0100

    Initial Commit

---
+++ RxDecodable/RxDecodable.h
@@ -0,0 +1,19 @@
+//
+//  RxDecodable.h
+//  RxDecodable
+//
+//  Created by Alejandro Perezpayá on 1/18/16.
+//  Copyright © 2016 ermes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxDecodable.
+FOUNDATION_EXPORT double RxDecodableVersionNumber;
+
+//! Project version string for RxDecodable.
+FOUNDATION_EXPORT const unsigned char RxDecodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxDecodable/PublicHeader.h>
+
+
